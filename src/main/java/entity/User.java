b4243e5bfec3c99db9585b131328e39a7aa88985
unknown <quentinlanoe@gmail.com>
2020-10-26 23:20:44 +0100

    initial commit

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,45 @@
+package entity;
+
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
---
+++ target/classes/data-postgres.sql
@@ -0,0 +1,5 @@
+INSERT INTO users(name, email) VALUES('John', 'john@genre.com');
+INSERT INTO users(name, email) VALUES('Julie', 'julie@genre.com');
+INSERT INTO users(name, email) VALUES('Jennifer', 'jennifer@genre.com');
+INSERT INTO users(name, email) VALUES('Helen', 'helen@genre.com');
+INSERT INTO users(name, email) VALUES('Rachel', 'rachel@genre.com');
